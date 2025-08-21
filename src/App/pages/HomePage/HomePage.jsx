03cee89ffe0bb890fcd9fe55e91aaf6484e216a0
leonardo_Wer <name89213126414@gmail.com>
2025-08-21 21:47:04 +0300

    Features Section base

--- src/App/pages/HomePage/HomePage.jsx
+++ src/App/pages/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import HeroSection from "./sections/HeroSection/HeroSection.jsx"
 import AboutSection from "s/App/pages/HomePage/sections/AboutSection/AboutSection.jsx";
 import SportSection from "s/App/pages/HomePage/sections/SportSection/SportSection.jsx";
 import PlansSection from "s/App/pages/HomePage/sections/PlansSection/PlansSection.jsx";
+import FeaturesSection from "s/App/pages/HomePage/sections/FeaturesSection/FeaturesSection.jsx";
 
 function HomePage() {
     return (
@@ -10,7 +11,7 @@ function HomePage() {
             <AboutSection />
             <SportSection />
             <PlansSection />
-            <div className="spacer"></div>
+            <FeaturesSection />
         </div>
     )
 }
--- src/App/pages/HomePage/sections/FeaturesSection/FeaturesSection.jsx
+++ src/App/pages/HomePage/sections/FeaturesSection/FeaturesSection.jsx
@@ -1,6 +1,67 @@
+import "./FeaturesSection.css"
+
 function FeaturesSection() {
     return (
-        <section id="featuresSection"></section>
+        <section id="featuresSection">
+            <div className="features-content-container">
+                <div className="features__top">
+
+                    <div className="features__text-container">
+                        <div className="features-text-container__line">
+                            <span className="features-line__text">
+                                Приложение
+                            </span>
+                            <img src="img/icons/ruby_plank_achievement.png" alt="inline-icon" className="features-line__icon" data-scale/>
+                            <span className="features-line__text">
+                                наполненное
+                            </span>
+                        </div>
+                        <div className="features-text-container__line">
+                            <img src="img/icons/records_icon.png" alt="inline-icon" className="features-line__icon"/>
+                            <span className="features-line__text">
+                                Полезными функциями
+                            </span>
+                            <img src="img/icons/workouts_plan_icon.png" alt="inline-icon" className="features-line__icon"/>
+                        </div>
+                        <div className="features-text-container__line">
+                            <span className="features-line__text">
+                                Которые делают
+                            </span>
+                            <img src="logo/logo-bg.svg" alt="inline-icon" className="features-line__icon"/>
+                            <span className="features-line__text">
+                                Разницу
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="features-top__content-container">
+
+                        <div className="features-top__grid">
+
+                            <div className="features__img-container">
+                                <div className="features__img-wrapper">
+                                    <img src="" alt=""/>
+                                </div>
+                            </div>
+
+                            <div className="features__title-container">
+                                <img src="" alt="" className="features-title-container__icon"/>
+
+                                <h4 className="features-title-container__title"></h4>
+                            </div>
+
+                            <div className="features__description-container">
+                                <p className="features-description-container__text">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, aut dolores eligendi fugit id ipsam nobis pariatur voluptas? Atque error, iste molestiae mollitia repudiandae voluptatem voluptatibus. Blanditiis neque omnis ut!
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </section>
     )
 }
 
